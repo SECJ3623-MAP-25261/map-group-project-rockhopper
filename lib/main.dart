6a26d567fba018c9fe492d03b3f42d74ad4b83c5
Siti Nurul Amirah Binti Sheikh Sulaiman <148435516+Mirabhain@users.noreply.github.com>
2025-11-14 07:21:05 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(const PinjamTechApp());
+}
+
+class PinjamTechApp extends StatelessWidget {
+  const PinjamTechApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PinjamTech',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
